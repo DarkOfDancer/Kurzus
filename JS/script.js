e197725fa9e332008822e0ba8098ac57999a4b42
Molnár Zsófia <zsofi2002mrzf@gmail.com>
2020-04-25 16:38:00 +0200

    JS_1._save

---
+++ JS/script.js
@@ -0,0 +1,48 @@
+/*4+3;
+
+function sayHello() {
+    console.log("Hello");
+}
+sayHello();
+
+var sayBye= function(){
+    console.log("Bye");
+}
+sayBye();
+
+function sing() {
+    console.log("HAAAAA");
+    console.log("HEEEEEEEE");
+}
+sing();
+
+function sing2(){
+    console.log("lalalala");
+    console.log("lasdsad");
+}
+sing2();
+
+
+function sing(song) {
+    console.log(song);
+}
+
+sing("asdas");
+sing("asdas");
+sing("asdas");
+
+
+function multiply(a, b) {
+    if (a>10||b>10){
+        return "That's to hard";
+    }else{
+        return a*b;
+    }
+}
+multiply(5,10);
+*/
+function multiply(a, b) {
+    return a*b;
+}
+var total= multiply(5,10);
+alert(total);
