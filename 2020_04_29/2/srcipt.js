4e43216a6099c5874c354ea72d149fe1003f60dc
Molnár Zsófia <zsofi2002mrzf@gmail.com>
2020-04-29 14:50:57 +0200

    2020_04_29_14:50

--- 2020_04_29/2/srcipt.js
+++ 2020_04_29/2/srcipt.js
@@ -1,4 +1,4 @@
-/*scope
+/*//scope
 var b = "Can I access this?";
 function bb() {
     var a = "hello";
@@ -26,9 +26,48 @@ function funestFunction() {
 console.log("window",fun);
 funFunction();
 funerFunction();
-funestFunction();*/
+funestFunction();
 
 var fun=5;
 function funestFunction() {
     console.log(fun);
+}
+
+//condition ? expr1 : expr2;
+
+function isUserValid(bool) {
+    return bool;
+}
+
+var answer = isUserValid(true) ? "You may enter" : "Access Denied";   
+
+function condition() {
+    if (isUserValid(true)) {
+        return "You may enter";        
+    }else{
+        return "Access denied";
+    }
+}
+
+var answer2 = condition();
+*/
+
+function moveCommand(direction) {
+    var whatHappens;
+    switch(direction) {
+        case "forward":
+            whatHappens = "you encounter a monster";
+            break;
+        case "back":
+            whatHappens = "you arrived home";
+            break;
+        case "right":
+            whatHappens = "you found a river";
+            break;
+        case "left":
+                whatHappens = "please enter a valid direction";
+                break;
+        default:
+            whatHappens = "Please enter a valid direction";
+    }return whatHappens;
 }
