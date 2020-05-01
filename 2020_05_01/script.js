bc16ee19c29a30e7e067acf3aea3bbacd22c4764
Molnár Zsófia <zsofi2002mrzf@gmail.com>
2020-05-01 13:39:17 +0200

    2020_05_01_13:39

--- 2020_05_01/script.js
+++ 2020_05_01/script.js
@@ -1,4 +1,4 @@
-//Advanced Arrays
+/*//Advanced Arrays
 const array =[1, 2, 10, 16];
 
 const double =[];
@@ -22,3 +22,40 @@ const reduceArray = array.reduce((accumulator, num) => {
     return accumulator + num;
 }, 0);
 console.log('reduce', reduceArray);
+*/
+//reference type
+
+var object1= {value: 10};
+var object2= object1;
+var object3= {value: 10};
+//context cs scope
+
+const object4 = {
+    a: function() {
+        console.log(this);
+    }
+}
+//instantiation
+class Player {
+    constructor(name, type) {
+        console.log('player', this);
+        this.name = name;
+        this.type = type;
+    }
+    introduce () {
+        console.log(`Hi I am ${this.name}, I'm a ${this.type}`)
+    }
+};
+
+class Wizard extends Player{
+    constructor(name, type) {
+        super(name, type)
+        console.log('wizard', this);
+        
+    }
+    play() {
+        console.log(`Weeeee I'm a ${this.type}`)
+    }
+}
+const wizard1 = new Wizard('Shelly','Healer');
+const wizard2 = new Wizard('Shawn','Dark Magic');
