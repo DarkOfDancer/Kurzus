cf16f4d512592a494879ed5270849ce245dc84f8
Molnár Zsófia <zsofi2002mrzf@gmail.com>
2020-05-01 13:56:48 +0200

    2020_05_01_13:56

--- 2020_05_01/script.js
+++ 2020_05_01/script.js
@@ -22,7 +22,7 @@ const reduceArray = array.reduce((accumulator, num) => {
     return accumulator + num;
 }, 0);
 console.log('reduce', reduceArray);
-*/
+
 //reference type
 
 var object1= {value: 10};
@@ -59,3 +59,20 @@ class Wizard extends Player{
 }
 const wizard1 = new Wizard('Shelly','Healer');
 const wizard2 = new Wizard('Shawn','Dark Magic');
+*/
+let obj = {
+    a: 'a',
+    b: 'b',
+    c: {
+        deep:'try and copy me'
+    }
+};
+let clone = Object.assign({}, obj);
+let clone2 = {...obj};
+let superClone = JSON.parse(JSON.stringify(obj));
+
+obj.c.deep="hahaha";
+console.log(obj);
+console.log(clone);
+console.log(clone2);
+console.log(superClone);
